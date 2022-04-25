01e42ee1d10f1ec81a8afa8823c7bb4b6fb40f76
mohadeseh <m.ghafoori78@gmail.com>
2022-04-25 20:02:12 +0430

    mask image

--- filters.py
+++ filters.py
@@ -1,4 +1,5 @@
 import cv2 as cv 
+import numpy as np
 img=cv.imread("G:\learn opencv/char.tif")
 cv.imshow("original image", img)
 #averaging
@@ -13,4 +14,11 @@ cv.imshow("median blur", median)
 #bilateral filter
 bilateral=cv.bilateralFilter(img,20,160,150) #blur image but keeps edges fairly sharp 
 cv.imshow("bilateral filter blur", bilateral)
+#masking
+blank=np.zeros(img.shape[:2],dtype="uint8")
+cicle=cv.circle(blank.copy(),(img.shape[0]//2,img.shape[1]//2),100,255,-1)
+recrangle=cv.rectangle(blank.copy(),(150,150),(400,400),255,-1)
+mask=cv.bitwise_and(cicle,recrangle)
+masked_img=cv.bitwise_and(img,img,mask=mask)
+cv.imshow("masked image", masked_img)
 cv.waitKey(0)

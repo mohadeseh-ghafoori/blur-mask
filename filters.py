ad03adcce0bad577c3cf920fc34ee84c689409b9
mohadeseh <m.ghafoori78@gmail.com>
2022-04-25 19:18:20 +0430

    blur image by median

--- filters.py
+++ filters.py
@@ -7,4 +7,7 @@ cv.imshow("average blur", average)
 #Gaussian filter
 gauss=cv.GaussianBlur(img,(7,7),0)
 cv.imshow("Gaussian blur", gauss)
+#median 
+median=cv.medianBlur(img,7) 
+cv.imshow("median blur", median)
 cv.waitKey(0)

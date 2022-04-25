bfb5e5f64afd7bce7777da9da4b240ed3cd67dac
mohadeseh <m.ghafoori78@gmail.com>
2022-04-25 19:08:21 +0430

    blur image by averaging

--- filters.py
+++ filters.py
@@ -1,4 +1,7 @@
 import cv2 as cv 
 img=cv.imread("G:\learn opencv/char.tif")
 cv.imshow("original image", img)
+#averaging
+average=cv.blur(img,(3,3))
+cv.imshow("average blur", average)
 cv.waitKey(0)

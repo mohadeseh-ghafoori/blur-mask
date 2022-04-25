f2eea85507166ee34e9c1293655585a06cac0bb7
mohadeseh <m.ghafoori78@gmail.com>
2022-04-25 19:12:51 +0430

    blur image by Gaussian filter

--- filters.py
+++ filters.py
@@ -2,6 +2,9 @@ import cv2 as cv
 img=cv.imread("G:\learn opencv/char.tif")
 cv.imshow("original image", img)
 #averaging
-average=cv.blur(img,(3,3))
+average=cv.blur(img,(7,7))
 cv.imshow("average blur", average)
+#Gaussian filter
+gauss=cv.GaussianBlur(img,(7,7),0)
+cv.imshow("Gaussian blur", gauss)
 cv.waitKey(0)
